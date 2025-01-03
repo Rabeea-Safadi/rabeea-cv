0b6cd8863f2c7b696c49995f4e1f4ae8f47f8647
Rabeea <rabeeasafadi@gmail.com>
2025-01-03 13:10:52 +0200

    home page done

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,9 +1,41 @@
+import { HttpClient } from "@angular/common/http";
 import { Component } from "@angular/core";
+import { SharedModule } from "../../shared/shared.module";
 
 @Component({
     selector: "app-home-page",
-    imports: [],
+    imports: [SharedModule],
     templateUrl: "./home-page.component.html",
     styleUrl: "./home-page.component.scss"
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+    private pageContent?: PageContent;
+
+    constructor(private http: HttpClient) {}
+
+    ngOnInit() {
+        this.http
+            .get<PageContent>("https://rabeea-cv-default-rtdb.firebaseio.com/home.json")
+            .subscribe((response) => {
+                this.pageContent = {
+                    name: response.name,
+                    title: response.title,
+                    subtitle: response.subtitle,
+                    email: response.email,
+                    linkedin: response.linkedin
+                };
+            });
+    }
+
+    get PageContent() {
+        return this.pageContent;
+    }
+}
+
+interface PageContent {
+    name?: string;
+    title?: string;
+    subtitle?: string;
+    email?: string;
+    linkedin?: string;
+}
