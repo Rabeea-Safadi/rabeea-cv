c0701f39df59338e6e5517b955f80a1738c3cb7c
Rabeea <rabeeasafadi@gmail.com>
2025-01-03 13:11:12 +0200

    added image to home page and finished about page

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { SharedModule } from "../../shared/shared.module";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-about-page',
-  imports: [],
-  templateUrl: './about-page.component.html',
-  styleUrl: './about-page.component.scss'
+    selector: "app-about-page",
+    imports: [SharedModule],
+    templateUrl: "./about-page.component.html",
+    styleUrl: "./about-page.component.scss"
 })
 export class AboutPageComponent {
+    private pageContent?: PageContent;
 
+    constructor(private http: HttpClient) {}
+
+    ngOnInit() {
+        this.http
+            .get<PageContent>("https://rabeea-cv-default-rtdb.firebaseio.com/about.json")
+            .subscribe((response) => {
+                this.pageContent = {
+                    bio: response.bio,
+                    education: response.education
+                };
+
+                console.log(response);
+            });
+    }
+
+    get PageContent() {
+        return this.pageContent;
+    }
+}
+interface PageContent {
+    bio?: string;
+    education?: string[];
 }
