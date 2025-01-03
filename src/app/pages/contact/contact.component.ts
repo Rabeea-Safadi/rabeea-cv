9b6763cf6ee29603006944d58d07c1185cbf2b21
Rabeea <rabeeasafadi@gmail.com>
2025-01-03 13:41:03 +0200

    contact page finished

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { SharedModule } from "../../shared/shared.module";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-contact',
-  imports: [],
-  templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+    selector: "app-contact",
+    imports: [SharedModule],
+    templateUrl: "./contact.component.html",
+    styleUrl: "./contact.component.scss"
 })
 export class ContactComponent {
+    private pageContent?: PageContent;
 
+    constructor(private http: HttpClient) {}
+
+    ngOnInit() {
+        this.http
+            .get<PageContent>("https://rabeea-cv-default-rtdb.firebaseio.com/home.json")
+            .subscribe((response) => {
+                this.pageContent = {
+                    email: response.email,
+                    linkedin: response.linkedin
+                };
+            });
+    }
+
+    get PageContent() {
+        return this.pageContent;
+    }
+}
+interface PageContent {
+    email?: string;
+    linkedin?: string;
 }
